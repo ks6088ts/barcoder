5c2cf352206c25e5ebe2c9c0b04f483f199586ad
ks6088ts <ks6088ts@gmail.com>
2022-07-15 08:02:43 +0900

    refer to version info

--- cmd/root.go
+++ cmd/root.go
@@ -22,23 +22,18 @@ THE SOFTWARE.
 package cmd
 
 import (
+	"fmt"
 	"os"
 
+	"github.com/ks6088ts/barcoder/internal"
 	"github.com/spf13/cobra"
 )
-
-
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "barcoder",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: fmt.Sprintf("barcoder: Revision=%v, Version=%v", internal.Revision, internal.Version),
+	Long:  fmt.Sprintf("barcoder: Revision=%v, Version=%v", internal.Revision, internal.Version),
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
@@ -64,5 +59,3 @@ func init() {
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
-
-
