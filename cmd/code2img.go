b7912ae22fc2df2caabda9545afbdb857ba20061
ks6088ts <ks6088ts@gmail.com>
2022-07-16 10:37:12 +0900

    support multiple barcode types

--- cmd/code2img.go
+++ cmd/code2img.go
@@ -22,6 +22,7 @@ THE SOFTWARE.
 package cmd
 
 import (
+	"fmt"
 	"image/png"
 	"log"
 	"os"
@@ -29,8 +30,43 @@ import (
 	"github.com/spf13/cobra"
 
 	"github.com/boombuler/barcode"
+	"github.com/boombuler/barcode/codabar"
+	"github.com/boombuler/barcode/code128"
+	"github.com/boombuler/barcode/code39"
+	"github.com/boombuler/barcode/code93"
+	"github.com/boombuler/barcode/datamatrix"
+	"github.com/boombuler/barcode/ean"
 	"github.com/boombuler/barcode/qr"
+	"github.com/boombuler/barcode/twooffive"
 )
+
+func createBarcode(barcodeType string, code string) (barcode.Barcode, error) {
+	if barcodeType == "codabar" {
+		return codabar.Encode(code)
+	}
+	if barcodeType == "code128" {
+		return code128.Encode(code)
+	}
+	if barcodeType == "code39" {
+		return code39.Encode(code, true, true)
+	}
+	if barcodeType == "code93" {
+		return code93.Encode(code, true, true)
+	}
+	if barcodeType == "datamatrix" {
+		return datamatrix.Encode(code)
+	}
+	if barcodeType == "ean" {
+		return ean.Encode(code)
+	}
+	if barcodeType == "qr" {
+		return qr.Encode(code, qr.M, qr.Auto)
+	}
+	if barcodeType == "twooffive" {
+		return twooffive.Encode(code, true)
+	}
+	return nil, fmt.Errorf("barcode type %s is not supported", barcodeType)
+}
 
 // code2imgCmd represents the code2img command
 var code2imgCmd = &cobra.Command{
@@ -54,27 +90,28 @@ var code2imgCmd = &cobra.Command{
 		if err != nil {
 			log.Fatalf("failed to parse `output`: %v", err)
 		}
-
-		// Create the QR code
-		qrCode, err := qr.Encode(code, qr.M, qr.Auto)
+		barcodeType, err := cmd.Flags().GetString("type")
 		if err != nil {
-			log.Fatalf("failed to crate a QR code: %v", err)
+			log.Fatalf("failed to parse `type`: %v", err)
 		}
 
-		qrCode, err = barcode.Scale(qrCode, width, height)
+		b, err := createBarcode(barcodeType, code)
 		if err != nil {
-			log.Fatalf("failed to scale a QR barcode: %v", err)
+			log.Fatalf("failed to crate a barcode: %v", err)
 		}
 
-		// create the output file
+		b, err = barcode.Scale(b, width, height)
+		if err != nil {
+			log.Fatalf("failed to scale a barcode: %v", err)
+		}
+
 		file, err := os.Create(output)
 		if err != nil {
 			log.Fatalf("failed to create an output file: %v", err)
 		}
 		defer file.Close()
 
-		// encode the barcode as png
-		if err = png.Encode(file, qrCode); err != nil {
+		if err = png.Encode(file, b); err != nil {
 			log.Fatalf("failed to encode the barcode as png: %v", err)
 		}
 	},
@@ -86,5 +123,6 @@ func init() {
 	code2imgCmd.Flags().StringP("code", "c", "code", "code2img")
 	code2imgCmd.Flags().IntP("width", "w", 200, "width of output image")
 	code2imgCmd.Flags().IntP("height", "e", 200, "height of output image")
-	code2imgCmd.Flags().StringP("output", "o", "qrcode.png", "path to output image")
+	code2imgCmd.Flags().StringP("output", "o", "qr.png", "path to output image")
+	code2imgCmd.Flags().StringP("type", "t", "qr", "barcode type (codabar|code128|code39|code93|datamatrix|ean|qr|twooffive)")
 }
