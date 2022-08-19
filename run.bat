5fb0075b1b1393a848804946fbec58721a1a735e
ks6088ts <ks6088ts@gmail.com>
2022-08-19 15:09:27 +0900

    generate markdown file

--- run.bat
+++ run.bat
@@ -1,7 +1,9 @@
 @echo off
 
-mkdir images
+rmdir /s /q generated
+mkdir generated
 
-for /f "tokens=1 delims=," %%i in (code_list.csv) do (
-  barcoder.exe code2img --height 100 --width 500 --type code128 --output images/%%i.png --code %%i
+for /f "tokens=1,2 delims=," %%i in (code_list.csv) do (
+  barcoder.exe code2img --height 100 --width 500 --type code128 --output generated/%%i.png --code %%i
+  echo %%i %%j ^<img src="./%%i.png" width="100%%"^> >> generated/sheet.md
 )
